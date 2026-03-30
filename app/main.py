1699ac906dd6e42118f7a43bfd94c33c79b3cbd6
andSLASH <zaharov.andrey.93@gmail.com>
2026-03-30 13:50:15 +0300

    'update'

--- app/main.py
+++ app/main.py
@@ -6,13 +6,13 @@ def cache(func: Callable) -> Callable:
     storage = {}
 
     @functools.wraps(func)
-    def wrapper(*args: Any) -> Any:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         if args in storage:
             print("Getting from cache")
             return storage[args]
 
         print("Calculating new result")
-        result = func(*args)
+        result = func(*args, **kwargs)
         storage[args] = result
         return result
 
